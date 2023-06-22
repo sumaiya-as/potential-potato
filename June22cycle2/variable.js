1e91cea8b6696112297db983c9d70a88bdda1d88
sumaiya-as <work.sumaiyaa@gmail.com>
2023-06-22 18:45:04 +0600

    declaration of variable

---
+++ June22cycle2/variable.js
@@ -0,0 +1,19 @@
+const variable1 = 10;
+const variable2 = "soma";
+const variable3 = variable2;
+const variable4 = {
+    variableType : "object",
+    variableValue : "value",
+};
+const variable5 = (function(){
+    return "Hello! my name is ";
+})();
+const variable6 = variable5 + variable2;
+console.log(variable1);
+console.log(variable2);
+console.log(variable3);
+console.log(variable4);
+console.log(variable5);
+console.log(variable6);
+
+
