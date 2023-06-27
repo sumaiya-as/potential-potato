0470dbfb27796b02ba915caee7e3e58c79d863f6
sumaiya-as <work.sumaiyaa@gmail.com>
2023-06-27 15:31:16 +0600

    solved problem 16,17,18

---
+++ june27challengesPart1/pronlem17.js
@@ -0,0 +1,18 @@
+/* Write a function that accepts an integer n and a string s as parameters,
+and returns a string of s repeated exactly n times.
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello" */
+
+function repeatStr (n, s) {
+    let newString = '';
+    for(i = 0; i < n; i++){
+      newString = s + newString;
+    }
+    return newString;
+  }
+
+  //using a built in method
+  function repeatStr (n, s) {
+    return s.repeat(n);
+  }
